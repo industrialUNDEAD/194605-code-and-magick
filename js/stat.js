a247b9af41154dfd4a2fa7abea410def3496467e
industrialUNDEAD <web.update@mail.ru>
2018-11-24 00:02:04 +0300

    Итоговый вариант 2-го задания

--- js/stat.js
+++ js/stat.js
@@ -1,31 +1,60 @@
-var CLOUD-WIDTH = 420;
+var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_FONT = '16px PT Mono';
-var HISTOGRAM_HEIGHT = 150;
 var COLUMN_WIDTH = 40;
+var COLUMN_HEIGHT = -150;
+var COLUMN_X = 140;
+var COLUMN_Y = 210;
 var COLUMN_GAP = 50;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_SHADOW_X = 110;
 var CLOUD_SHADOW_Y = 20;
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP; // надо подумать как изменить переменную под задачу
+var TEXT_Y = 230;
+var TEXT_ALERT = 30;
+var TEXT_ALERT_GAP = 20;
 
-var renderCloud = function(ctx, x, y) {
+var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD-WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 }
 
-window.renderStatistics = function (ctx, names, times) {
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_SHADOW_X, CLOUD_SHADOW_Y, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
-  ctx.fillText('Вы', 110, 75);
-  ctx.fillRect(110, 60, 40, 150);
+  ctx.font = CLOUD_FONT;
+  ctx.fillText('Ура вы победили!', COLUMN_X, TEXT_ALERT);
+  ctx.fillText('Список результатов:', COLUMN_X, TEXT_ALERT + TEXT_ALERT_GAP);
 
-  ctx.fillText('Иван', 110, 105);
-  ctx.fillRect(160, 90, 40, 20);
+  ctx.fillStyle = '#000';
 
-  ctx.fillText('Юлия', 110, 135);
-  ctx.fillRect(160, 120, 40, 20);
+  var playerIndex = 0;
+  var playerName = 'Вы';
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = ('#000');
+    ctx.fillText(players[i], COLUMN_X + ((COLUMN_WIDTH + COLUMN_GAP) * i), TEXT_Y);
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = ('rgba(255, 0, 0, 1)');
+    } else {
+      ctx.fillStyle = ('blue');
+    }
+    ctx.fillRect(COLUMN_X + ((COLUMN_WIDTH + COLUMN_GAP) * i ), COLUMN_Y, COLUMN_WIDTH, (COLUMN_HEIGHT * times[i]) / maxTime);
+  }
 }
